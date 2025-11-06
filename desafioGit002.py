101e6dbb003352aeae2e925463d2fa6377eef1f7
Olivia Vilela <olivia.tech.vilela@gmail.com>
2025-11-06 10:17:22 -0300

    Coloquei tipo Float pra pegar números reais, coloquei as respostas com dois decimais pós vírgula

--- desafioGit002.py
+++ desafioGit002.py
@@ -5,8 +5,8 @@ print(f'{'CALCULADORA':^40}')
 print('-='*20)
 
 while True:
-    n1 = int(input('Digite o valor: '))
-    n2 = int(input('Digite o valor: '))
+    n1 = float(input('Digite o valor: '))
+    n2 = float(input('Digite o valor: '))
     print('DIGITE [1] PARA SOMAR')
     print('DIGITE [2] PARA SUBTRAIR')
     print('DIGITE [3] PARA MULTIPLICAR')
@@ -16,19 +16,19 @@ while True:
 
     if op == '1':
         soma = n1 + n2
-        print(f'{n1} + {n2} é igual a {soma}')
+        print(f'{n1} + {n2} é igual a {soma:.2f}')
     elif op == '2':
         subtrair = n1 - n2
-        print(f' {n1} - {n2} é igual a {subtrair}')
+        print(f' {n1} - {n2} é igual a {subtrair:.2f}')
     elif op == '3':
         multiplicar = n1 * n2
-        print(f'{n1} * {n2} é igual a {multiplicar}')
+        print(f'{n1} * {n2} é igual a {multiplicar:.2f}')
     elif op == '4':
         dividir = n1 / n2
-        print(f'{n1} / {n2} é igual a {dividir}')
+        print(f'{n1} / {n2} é igual a {dividir:.2f}')
     elif op == '5':
         elevar = pow(n1,n2)
-        print(f'{n1} elevado a {n2} é igual a {elevar}')
+        print(f'{n1} elevado a {n2} é igual a {elevar:.2f}')
     else:
         print('Comando Inválido')
     novamente = input('Deseja realizar novamente? [S/N] ').upper()
